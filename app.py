67034c62628a08f046a94cc7c9139e4e9696d383
Zane Sterling <sterling.zane@gmail.com>
2013-11-16 13:54:41 -0500

    Add searchbar redirection.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import request, render_template
+from flask import request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -7,6 +7,17 @@ app = Flask(__name__)
 def home():
 	if request.method == "GET":
 		return render_template('home.html')
+
+	#POST, i.e. search
+	search_request = request.form['search-request']
+	return redirect(url_for('search', search_request=search_request))
+
+@app.route('/search/<search_request>')
+def search(search_request=None):
+	if not search_request:
+		return redirect('home')
+
+	return render_template('search.html', search_request=search_request)
 
 if __name__ == '__main__':
 	app.debug = True
