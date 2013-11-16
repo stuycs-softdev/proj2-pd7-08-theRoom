b7321f962aee93358bec0c8c026cf8158bd23970
Josh Hofing <joshhofing@gmail.com>
2013-11-16 14:30:10 -0500

    oh god generating trigrams eww shoot me

--- reviewer.py
+++ reviewer.py
@@ -1,4 +1,4 @@
-
+from random import choice
 # our magical, automated reviewer
 
 
@@ -25,3 +25,30 @@ def weightedListofKeys(corpus):
             out.append(key)
     return out
 
+
+# I should be shot for writing this code
+def generateCorpus(sentence):
+    words = sentence.split(' ')
+    d = {}
+    for i in range(len(words)):
+        word = words[i]
+        if word in d:
+            d[word]['weight'] += 1
+        else:
+            d[word] = {'weight':1}
+        if len(words)-1 > i:
+            nextword = words[i+1]
+            if nextword in d[word]:
+                d[word][nextword]['weight']+=1
+            else:
+                d[word][nextword] = {'weight':1}
+            if len(words)-2 > i:
+                nextnextword = words[i+2]
+                if nextnextword in d[word][nextword]:
+                    d[word][nextword][nextnextword]['weight'] += 1
+                else:
+                    d[word][nextword][nextnextword] = {'weight':1}
+    print d
+
+
+generateCorpus("the quick brown fox jumps over the lazy dog")
