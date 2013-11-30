50ce68c91b7e3cc48bdd47293709341dd2572667
hunteriam <theprinceofhesh@gmail.com>
2013-11-30 00:26:43 -0500

    schemas

--- db.py
+++ db.py
@@ -1,9 +1,20 @@
 import sqlite3
 
 DATABASE = "author.db"
-SCHEMA = """
+SCHEMA_DOUBLES = """
+pid int primaryKey,
+firstWord varchar(255),
+secondWord varchar(255),
+UNIQUE(firstWord, secondWord)
 """
-
+SCHEMA_THIRD = """
+pid int primaryKey,
+src int foreignKey,
+word varchar(255),
+freq int,
+coherency int,
+style int
+"""
 createTable = "CREATE TABLE %s(%s);"
 insertInto = "INSERT INTO %s(%s) VALUES(%s)";
 
