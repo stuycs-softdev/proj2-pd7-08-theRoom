ab814da858d08bfbf3a88bec340920676584cad2
System Administrator <root@Denise-Shirleys-MacBook-Air.local>
2013-12-01 17:44:34 -0500

    works

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import sqlite3
 class Author:
-
+	table = None
 	sid = None
 	db = None
 	
@@ -30,26 +30,42 @@ class Author:
 	increment = "UPDATE %s SET %s = %s + %s WHERE %s = %s"
 	def getDB(self):
 		if self.db == None:
-			return sqlite3.connect(DATABASE)
-		else:
-			return 
+			self.db = sqlite3.connect(self.DATABASE)
+		return self.db
+	def select(self,table,id):
+		self.sid = id
+		self.table = table
+		i = self.db.execute("SELECT * FROM %s WHERE pid = %s"%(table,id))
+		return i.fetchone()
+	def getWords(self):
+		i = self.db.execute("SELECT * FROM %s WHERE src = %s"%(self.TABLE_CHOICES,self.sid))
+		return i.fetchall()
+	def selectWords(a,b):
+		i = self.db.execute("SELECT * FROM %s WHERE firstWord = %s &  secondWord = %s"%(self.TABLE_PAIRS,a,b))
+		data = i.fetchone()
+		self.sid = data[0]
+		self.table = self.TABLE_PAIRS
+		return data
 	def reset(self,db):
 		self.dropTable("*",db)
+		return self
 	def dropTable(self,table,db):
 		db.execute(self.dropTable%table)
+		return self
 	def initPairs(self,name,db):
 		db.execute(self.createTable%(name,self.SCHEMA_PAIRS))
-		return db
+		return self
 	def initChoices(self,name,db):
 		db.execute(self.createTable%(name,self.SCHEMA_CHOICES))
-		return db
+		return self
 	def init(self):
 		db = self.getDB()
-		self.initPairs(TABLE_PAIRS,db)
-		self.initChoices(TABLE_CHOICES,db)
-		return db
-	def increment(self,val,key):
-		self.db.execute(self.increment
+		self.initPairs(self.TABLE_PAIRS,db)
+		self.initChoices(self.TABLE_CHOICES,db)
+		return self
+	def add(self,val,key):
+		self.db.execute(self.increment%(table,key,key,val,"pid",sid));
+		return self
 if __name__ == "__main__":
 	a = Author()
 	print a.getDB()
