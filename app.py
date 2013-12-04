edaefcaa6328280ec8de9eacbc7212bce290856c
Josh Hofing <joshhofing@gmail.com>
2013-12-04 13:12:18 -0500

    Add the sentence generation on the page. Reliant on the db update hunter is writing now.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request, render_template, redirect, url_for
 import rottenapi
 import reviewer
+import db
 
 app = Flask(__name__)
 
@@ -29,12 +30,17 @@ def search(search_request=None):
 def movie(movie_id):
     movieInfo = rottenapi.movieInfo(movie_id)
     castInfo = rottenapi.castInfo(movie_id) 
-    print rottenapi.reviews(movie_id)
-    reviews = [rottenapi.reviewText(r) for r in rottenapi.reviews(movie_id)['reviews']]
-
+    reviews = reviewText(rottenapi.reviews['reviews'])
+    author = db.Author()
+    for review in reviews:
+        r = reviewText(review).split(' ')
+        for i in range(len(r)-3):
+            author.insert(r[i], r[i+1], r[i+2])
+    
+    text = reviewer.generateSentence(author.everything())
 
     similarMovies = rottenapi.similarMovies(movie_id)
-    return render_template('movie.html', movieInfo=movieInfo, castInfo=castInfo, reviews=reviews, similarMovies=similarMovies)
+    return render_template('movie.html', movieInfo=movieInfo, castInfo=castInfo, text=text, similarMovies=similarMovies)
 
 if __name__ == '__main__':
     app.debug = True
