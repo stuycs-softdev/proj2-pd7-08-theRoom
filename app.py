e11e85a4270d724628b78ecb5ac3710f32b2cb56
Josh Hofing <joshhofing@gmail.com>
2013-12-04 12:54:38 -0500

    Temp

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def search(search_request=None):
 def movie(movie_id):
 	movieInfo = rottenapi.movieInfo(movie_id)
 	castInfo = rottenapi.castInfo(movie_id)
+    print rottenapi.reviews(movie_id)
 	reviews = [rottenapi.reviewText(r) for r in rottenapi.reviews(movie_id)]
 
 
