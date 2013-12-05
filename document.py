f7e8386bafd170f3bf6ebac2ffc81204e5684ebe
hunteriam <hhstuy@gmail.com>
2013-12-04 19:53:27 -0500

    updated keycodes

--- document.py
+++ document.py
@@ -1,10 +1,6 @@
 class doc():
-	startd = 0
 	starts = 1
-	endd = 2
 	ends = 3
 	
-	SQLsd = "\\[$$0\\]"
-	SQLss = "\\[$$1\\]"
-	SQLed = "\\[$$2\\]"
-	SQLes = "\\[$$3\\]"
+	SQLss = "\[$$1\]"
+	SQLes = "\[$$3\]"
